11d310b67eeff603854ca109dd893624f185e16a
gib <richard@futrli.com>
2019-07-04 11:49:59 +0100

    syntax error

--- link-package.rb
+++ link-package.rb
@@ -3,7 +3,7 @@ class LinkPackage < Formula
   homepage "https://github.com/lifeglug/homebrew-link-package"
   url "https://github.com/lifeglug/homebrew-link-package/tarball/4e6908c21ac3bb7e8bd8875b6d628952f20d7975"
   version "0.0.2"
-  sha256 "732359d10fefa37cd8096c7af6f0bb2e88acffe3070effdabf49620ff0399f16
+  sha256 "732359d10fefa37cd8096c7af6f0bb2e88acffe3070effdabf49620ff0399f16"
 
   def install
     bin.install "./bin/link-package"
