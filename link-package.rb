3196ed2a3615df863e1b1d23f2f1b2661947aa7d
gib <richard@futrli.com>
2019-07-04 11:43:07 +0100

    updating link-package

--- link-package.rb
+++ link-package.rb
@@ -3,7 +3,7 @@ class LinkPackage < Formula
   homepage "https://github.com/lifeglug/homebrew-link-package"
   url "https://github.com/lifeglug/homebrew-link-package/tarball/4e6908c21ac3bb7e8bd8875b6d628952f20d7975"
   version "0.0.2"
-  sha "f86a67b268361fe12cc848aaf1c7482eaaf4543e21f8c5dc6ad5fef9814cdaee"
+  sha256 "f86a67b268361fe12cc848aaf1c7482eaaf4543e21f8c5dc6ad5fef9814cdaee"
 
   def install
     bin.install "./bin/link-package"
