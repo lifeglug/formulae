eb3c3541ff54318c5b0b1547dc3a774d183159f1
gib <richard@futrli.com>
2019-07-23 10:02:52 +0100

    link-package 0.0.3

--- link-package.rb
+++ link-package.rb
@@ -1,9 +1,9 @@
 class LinkPackage < Formula
   desc "Simply bash script to link build distributables"
-  homepage "https://github.com/lifeglug/homebrew-link-package"
-  url "https://github.com/lifeglug/homebrew-link-package/tarball/4e6908c21ac3bb7e8bd8875b6d628952f20d7975"
-  version "0.0.2"
-  sha256 "732359d10fefa37cd8096c7af6f0bb2e88acffe3070effdabf49620ff0399f16"
+  homepage "https://github.com/lifeglug/link-package"
+  url "https://github.com/lifeglug/link-package/tarball/0c8bfb5c04d10df65ccf9dd01dff036ea3b3528c"
+  version "0.0.1"
+  sha256 "381c94125bf03ab3107e052d33f265081cb09b38e028d41410320023a48ce828"
 
   def install
     bin.install "./bin/link-package"
