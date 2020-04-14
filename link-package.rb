7b411a6c9cb7c704490ca689515b8c4ee21ae654
gib <richard@futrli.com>
2020-04-14 10:18:17 +0100

    update link-package

--- link-package.rb
+++ link-package.rb
@@ -1,8 +1,8 @@
 class LinkPackage < Formula
   desc "Simply bash script to link build distributables"
   homepage "https://github.com/lifeglug/link-package"
-  url "https://api.github.com/repos/lifeglug/link-package/tarball/v3.0.0"
-  version "3.0.0"
+  url "https://api.github.com/repos/lifeglug/link-package/tarball/v3.0.1"
+  version "3.0.1"
   sha256 "ccf341700b1877889e8ad60a21b61ca4fbf9b8e36f47875295b72b2d0575badc"
 
   def install
