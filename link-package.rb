a4ea6f7805973d74ef8d515d60ec80b98aa4044e
gib <richard@futrli.com>
2020-04-14 10:18:59 +0100

    fix sha

--- link-package.rb
+++ link-package.rb
@@ -3,7 +3,7 @@ class LinkPackage < Formula
   homepage "https://github.com/lifeglug/link-package"
   url "https://api.github.com/repos/lifeglug/link-package/tarball/v3.0.1"
   version "3.0.1"
-  sha256 "ccf341700b1877889e8ad60a21b61ca4fbf9b8e36f47875295b72b2d0575badc"
+  sha256 "bb089e4bf7a06aa6f776f4d3e5969c2fd9e34245dc74358e7e7327f1f8dfaad2"
 
   def install
     bin.install "./bin/link-package"
